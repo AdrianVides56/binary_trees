aa61fc7015eacac84c7fc68f85ff24c58dae06cf
AdrianVides56 <adrianvj18@gmail.com>
2021-03-09 11:34:52 -0500

    Initializing variables in NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,13 +10,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = NULL;
 
 	if (parent == NULL)
 		return (NULL);
 
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
 	new->parent = parent;
 	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
 
 	if (parent->left != NULL)
 	{
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,13 +10,19 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = NULL;
 
 	if (parent == NULL)
 		return (NULL);
 
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
 	new->parent = parent;
 	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
 
 	if (parent->right != NULL)
 	{

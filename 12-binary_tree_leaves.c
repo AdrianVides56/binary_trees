e17cef6177e301df291c2f47d0edcefc7e969a8d
AdrianVides56 <adrianvj18@gmail.com>
2021-03-10 09:32:48 -0500

    Changed declaration of position

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,11 +8,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaves = 0, right_leaves = 0;
 	if (tree == NULL)
 		return (0);
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);

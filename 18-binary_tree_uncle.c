04c42b29b4daeb31f6e1f28c22fc4fe6fff36b6b
AdrianVides56 <adrianvj18@gmail.com>
2021-03-10 15:49:53 -0500

    Revert "Added two conditionals more"
    This reverts commit bd309186a99db1facae2bc18d70101af21750ebc.

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,14 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent->left->left == node->parent->left &&
 	    node->parent->parent->right != NULL)
 		return (node->parent->parent->right);
-	if (node->parent->parent->left->right == node->parent->right &&
-	    node->parent->parent->right != NULL)
-		return (node->parent->parent->right);
-
 	if (node->parent->parent->right->right == node->parent->right &&
-	    node->parent->parent->left != NULL)
-		return (node->parent->parent->left);
-	if (node->parent->parent->right->left == node->parent->left &&
 	    node->parent->parent->left != NULL)
 		return (node->parent->parent->left);
 	return (NULL);

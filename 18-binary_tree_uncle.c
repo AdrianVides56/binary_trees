1e2286fdf3ebeda235284be78f4e9071b34a1544
AdrianVides56 <adrianvj18@gmail.com>
2021-03-10 15:51:43 -0500

    Changed conditional statement

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,10 +9,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-	if (node->parent->parent->left->left == node->parent->left &&
+	if (node->parent->parent->left == node->parent &&
 	    node->parent->parent->right != NULL)
 		return (node->parent->parent->right);
-	if (node->parent->parent->right->right == node->parent->right &&
+	if (node->parent->parent->right == node->parent&&
 	    node->parent->parent->left != NULL)
 		return (node->parent->parent->left);
 	return (NULL);
